772089d8c14c408f701aa3c78bad95a4aea96757
Dawid T <geekoonn@gmail.com>
2023-03-28 19:29:11 +0200

    add posibility search by meal name

--- script/index.js
+++ script/index.js
@@ -7,27 +7,53 @@ document.addEventListener("DOMContentLoaded", () => {
   const activitiesSearch = document.querySelector(".activitiesSearch");
   const activities = document.querySelector(".activities");
   const catCard = document.querySelector(".categoryCard");
+  const search = document.querySelector(".search");
+  const inputSearch = document.querySelector(".inputSearch");
   const footerText = document.querySelector(".footerText");
 
   CATEGORIES_URL = "https://www.themealdb.com/api/json/v1/1/categories.php";
 
+  let mealsList;
+
+  //* Set current year in footer
   const date = new Date();
   let year = date.getFullYear();
   footerText.innerHTML = `&copy; devTro ${year} All Rights Reserved.`;
 
-  //* DOWNLOADS CATEGORIES FROM API
+  //* HIDE WELCOME DIV
+  welcomeDiv.addEventListener(
+    "click",
+    (showMainView = () => {
+      console.log("welcome click, main view showed");
+      console.log(welcomeDiv.classList.contains("divOff"));
+      if (welcomeDiv.className !== "divOff") {
+        welcomeDiv.classList.remove("divOn");
+        welcomeDiv.classList.add("divOff");
+        mainView.classList.remove("divOff");
+        mainView.classList.add("divOn");
+      } else {
+      }
+    })
+  );
 
-  fetch(CATEGORIES_URL)
-    .then((res) => res.json())
-    .then((data) => {
-      data.categories.map((category) => {
-        //console.log(category);
-        createCard(category);
+  //*SHOW CATEGORIES
+  activitiesCategories.addEventListener("click", () => {
+    //* DOWNLOADS CATEGORIES FROM API
+    fetch(CATEGORIES_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        data.categories.map((category) => {
+          //console.log(category);
+          createCard(category);
+        });
       });
-    });
+    console.log("categories clicked");
+    activities.classList.add("divOff");
+    categories.classList.remove("divOff");
+    categories.classList.add("divOn");
+  });
 
   //* CREATE CARDS FOR CATEGORIES
-
   let createCard = (category) => {
     //console.log(category);
     let categoryCard = document.createElement("div");
@@ -57,32 +83,35 @@ document.addEventListener("DOMContentLoaded", () => {
     //console.log(catCard);
   };
 
-  //* Hide welcome div
-
-  welcomeDiv.addEventListener(
-    "click",
-    (showMainView = () => {
-      console.log("welcome click, main view showed");
-      console.log(welcomeDiv.classList.contains("divOff"));
-      if (welcomeDiv.className !== "divOff") {
-        welcomeDiv.classList.remove("divOn");
-        welcomeDiv.classList.add("divOff");
-        mainView.classList.remove("divOff");
-        mainView.classList.add("divOn");
-      } else {
-      }
-    })
-  );
-
-  activitiesCategories.addEventListener("click", () => {
-    console.log("categories clicked");
-    activities.classList.add("divOff");
-    categories.classList.remove("divOff");
-    categories.classList.add("divOn");
-  });
-
+  //*SHOW SEARCH
   activitiesSearch.addEventListener("click", () => {
     console.log("search clicked");
     activities.classList.add("divOff");
+    search.classList.remove("divOff");
+    search.classList.add("divOn");
   });
+
+  //*SEARCH RECIPE BY NAME
+  inputSearch.addEventListener("keyup", (e) => {
+    mealName = e.target.value;
+    if (e.keyCode === 13 && inputSearch.value !== "")
+      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
+        .then((res) => res.json())
+        .then((data) => {
+          mealsList = data.meals;
+          inputSearch.value = "";
+          console.log(mealsList);
+
+          let mealDiv = document.createElement("div");
+          mealDiv.classList.add("searchMeals");
+          mealDiv.classList.add("centerFlex");
+          search.appendChild(mealDiv);
+
+          createCardMeal(mealsList);
+        });
+  });
+
+  let createCardMeal = (mealsList) => {
+    console.log(mealsList);
+  };
 });
